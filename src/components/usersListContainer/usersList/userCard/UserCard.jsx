d1b08883089c2c206f9bc56be2414de9b2abf3cd
IgnacioVivas <ignacab@hotmail.com>
2022-11-07 14:54:35 -0300

    mostrando si esta validado o no: incompleto

--- src/components/usersListContainer/usersList/userCard/UserCard.jsx
+++ src/components/usersListContainer/usersList/userCard/UserCard.jsx
@@ -1,6 +1,21 @@
+import axios from 'axios';
 import React from 'react';
 
 function UserCard({ user }) {
+  const validateUser = async (id, userId, title, body) => {
+    try {
+      const { data } = await axios.put(
+        `https://dpower-production.up.railway.app/users/${user.id}`,
+        {
+          validated: user.validated,
+        }
+      );
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  validateUser({ validated: !user.validated });
   return (
     <>
       <div className='celda-container'>
@@ -8,8 +23,8 @@ function UserCard({ user }) {
           <span>{user?.name}</span>
         </div>
         <div className='celda'>
-          <span>{user?.validated}</span>
-          <i className='ri-edit-2-line'></i>
+          <span>{user?.validated ? 'true' : 'false'}</span>
+          <i className='ri-edit-2-line' onClick={validateUser}></i>
         </div>
       </div>
     </>
